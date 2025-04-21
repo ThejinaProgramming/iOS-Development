ef4e7332a41e4353c445925f0c5d639e756938b8
Thejina <thejinaprogramming@gmail.com>
2025-04-22 03:11:39 +0530

    Initiation of application development

---
+++ PersonalBudgetManager/PersonalBudgetManager/PersonalBudgetManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalBudgetManagerApp.swift
+//  PersonalBudgetManager
+//
+//  Created by user271744 on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct PersonalBudgetManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PersonalBudgetManager/PersonalBudgetManagerTests/PersonalBudgetManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalBudgetManagerTests.swift
+//  PersonalBudgetManagerTests
+//
+//  Created by user271744 on 4/22/25.
+//
+
+import Testing
+@testable import PersonalBudgetManager
+
+struct PersonalBudgetManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
