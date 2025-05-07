fb4f1ef216bc95888d0a11c26b780508a6671bda
ThejinaProgramming <thejinaprogramming@gmail.com>
2025-05-07 23:40:09 +0800

    Added UI and unit tests

--- PersonalBudgetManager/PersonalBudgetManagerTests/PersonalBudgetManagerTests.swift
+++ PersonalBudgetManager/PersonalBudgetManagerTests/PersonalBudgetManagerTests.swift
@@ -5,13 +5,44 @@
 //  Created by user271744 on 4/22/25.
 //
 
-import Testing
+import XCTest
 @testable import PersonalBudgetManager
 
-struct PersonalBudgetManagerTests {
+final class PersonalBudgetManagerTests: XCTestCase {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    func testTotalExpenses() {
+        //Given
+        let expense1: Double = 1000
+        let expense2: Double = 2000
+        
+        //When
+        let result = expense1 + expense2
+        
+        //Then
+        XCTAssertEqual(result, 3000, "Calculation of total expenses failed")
     }
-
-}
+    
+    func testTotalIncome() {
+        //Given
+        let income1: Double = 2000
+        let income2: Double = 2000
+        
+        //When
+        let result = income1 + income2
+        
+        //Then
+        XCTAssertEqual(result, 4000, "Calculation of total income failed")
+    }
+    
+    func testTotalRemaining() {
+        //Given
+        let assets: Double = 5000
+        let income: Double = 1000
+        let expense: Double = 2000
+        
+        //When
+        let result = (assets + income) - expense
+        
+        //Then
+        XCTAssertEqual(result, 4000, "Calculation of remaining amount failed")
+    }}
--- PersonalBudgetManager/PersonalBudgetManagerUITests/PersonalBudgetManagerUITests.swift
+++ PersonalBudgetManager/PersonalBudgetManagerUITests/PersonalBudgetManagerUITests.swift
@@ -8,13 +8,14 @@
 import XCTest
 
 final class PersonalBudgetManagerUITests: XCTestCase {
+    let app = XCUIApplication()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
+        app.launch()
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
@@ -23,19 +24,24 @@ final class PersonalBudgetManagerUITests: XCTestCase {
     }
 
     @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testInitialScreen() throws {
+        //Given
         let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        // This measures how long it takes to launch your application.
-        measure(metrics: [XCTApplicationLaunchMetric()]) {
-            XCUIApplication().launch()
-        }
+        app.activate()
+        app/*@START_MENU_TOKEN@*/.buttons["Go Inside..."]/*[[".otherElements.buttons[\"Go Inside...\"]",".buttons.firstMatch",".buttons[\"Go Inside...\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app/*@START_MENU_TOKEN@*/.buttons["Sign Up"]/*[[".otherElements.buttons[\"Sign Up\"]",".buttons[\"Sign Up\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app/*@START_MENU_TOKEN@*/.buttons["Sign In"]/*[[".otherElements.buttons[\"Sign In\"]",".buttons[\"Sign In\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        //When
+        app/*@START_MENU_TOKEN@*/.textFields["your@email.com"]/*[[".otherElements.textFields[\"your@email.com\"]",".textFields.firstMatch",".textFields[\"your@email.com\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let secureTextField = app/*@START_MENU_TOKEN@*/.secureTextFields["********"]/*[[".otherElements.secureTextFields[\"********\"]",".secureTextFields.firstMatch",".secureTextFields[\"********\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        secureTextField.tap()
+        secureTextField.tap()
+        
+        //Then
+        app.buttons.matching(identifier: "Sign In").element(boundBy: 1).tap()
+        
     }
 }
+
+
